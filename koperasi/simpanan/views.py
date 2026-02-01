aac4e076f68cb51225e72578056e832d635afc7e
fath <rizkyalfath80@gmail.com>
2026-02-01 16:59:59 +0700

    fix

--- koperasi/simpanan/views.py
+++ koperasi/simpanan/views.py
@@ -324,8 +324,8 @@ def download_kwitansi(request, history_id):
     if trx.jenis_transaksi == HistoryTabungan.SETOR:
         judul = "BUKTI PENERIMAAN KAS"
         pihak_label = "Diterima dari"
-        pemberi = f"({anggota.nama})"
-        penerima = "(...........................)"
+        pemberi = "(...........................)"
+        penerima = f"({anggota.nama})"
         filename = f"kwitansi_setoran_{anggota.nama}"
 
     elif trx.jenis_transaksi == HistoryTabungan.TARIK:
