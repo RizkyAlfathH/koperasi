827e71fd64ae0fef5a8e7bf52ec79a1f7d582cff
naufal <naufalfauzann19@gmail.com>
2026-04-19 21:02:47 +0700

    hapus komen apps.py

--- koperasi/pinjaman/apps.py
+++ koperasi/pinjaman/apps.py
@@ -6,11 +6,11 @@ class PinjamanConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pinjaman"
 
-    # def ready(self):
-    #     import sys
-    #     # Jangan jalankan saat perintah migrate, makemigrations, shell, dll
-    #     excluded = ("migrate", "makemigrations", "shell", "test", "collectstatic")
-    #     if any(cmd in sys.argv for cmd in excluded):
-    #         return
-    #     from . import scheduler
-    #     scheduler.start()
+    def ready(self):
+        import sys
+        # Jangan jalankan saat perintah migrate, makemigrations, shell, dll
+        excluded = ("migrate", "makemigrations", "shell", "test", "collectstatic")
+        if any(cmd in sys.argv for cmd in excluded):
+            return
+        from . import scheduler
+        scheduler.start()
